79acb92bad1f435624261d2f2308ab7d9f0249e4
andy11181 <zhua15@mcmaster.ca>
2019-04-07 23:43:03 +0000

    Added Clear function

--- Project03/CubeMaker.elm
+++ Project03/CubeMaker.elm
@@ -11,7 +11,7 @@ g = 0.1
 a = 0.06
 friction = 0.03
 jumpStrength = 2 -- be reasonable, jump heights of over 75 will break collision detection
-platform = [((-20,-4),(-38,-36),black),((-4,30),(-39,-38),blue)]
+initialPlatforms = [((-20,-4),(-38,-36),black),((-4,30),(-39,-38),blue),((-10,10),(-16,-14),purple),((20,30),(6,8),orange),((-10,0),(20,22),green)]
 numplayers = 2 -- will increase player limit beyond 2 soon
 playerColours = ((purple,blue),(red,green))
 cubesize = 3
@@ -32,13 +32,16 @@ type alias Model =
         player2 : (Bool,Color),
         player3 : (Bool,Color),
         player4 : (Bool,Color),
-        velx : ((Float,Float),(Float,Float)),
-        vely : ((Float,Float),(Float,Float)),
-        posx : ((Float,Float),(Float,Float)),
-        posy : ((Float,Float),(Float,Float)),
-        response : String
+        velx : FourTuples,
+        vely : FourTuples,
+        posx : FourTuples,
+        posy : FourTuples,
+        response : String,
+        platform : PlatformTuples
     }
 
+type alias PlatformTuples = List((Float,Float),(Float,Float),Color)
+type alias FourTuples = ((Float,Float),(Float,Float))
 
 init : () -> Url.Url -> Key -> ( Model, Cmd Msg )
 init flags url key =    let
@@ -53,7 +56,8 @@ init flags url key =    let
                         vely = ((0,0),(0,0)),
                         posx = startposx,
                         posy = startposy,
-                        response = ""
+                        response = "",
+                        platform = initialPlatforms
                         }, Cmd.none) 
 
 
@@ -66,27 +70,27 @@ update msg model =
                 (isAlive2,colour2) = model.player2
                 (isAlive3,colour3) = model.player3
                 (isAlive4,colour4) = model.player4
-                velx1new = velxUpdate model.posx model.posy model.velx model.vely wasdX isAlive1 1
-                posx1new = posxUpdate model.posx model.posy model.velx model.vely isAlive1 1
-                vely1new = velyUpdate model.posx model.posy model.velx model.vely wasdY isAlive1 1
-                posy1new = posyUpdate model.posx model.posy model.velx model.vely isAlive1 1
-                velx2new = velxUpdate model.posx model.posy model.velx model.vely arrowX isAlive2 2
-                posx2new = posxUpdate model.posx model.posy model.velx model.vely isAlive2 2
-                vely2new = velyUpdate model.posx model.posy model.velx model.vely arrowY isAlive2 2
-                posy2new = posyUpdate model.posx model.posy model.velx model.vely isAlive2 2
-                velx3new = velxUpdate model.posx model.posy model.velx model.vely 0 isAlive3 3
-                posx3new = posxUpdate model.posx model.posy model.velx model.vely isAlive3 3
-                vely3new = velyUpdate model.posx model.posy model.velx model.vely 0 isAlive3 3
-                posy3new = posyUpdate model.posx model.posy model.velx model.vely isAlive3 3
-                velx4new = velxUpdate model.posx model.posy model.velx model.vely 0 isAlive4 4
-                posx4new = posxUpdate model.posx model.posy model.velx model.vely isAlive4 4
-                vely4new = velyUpdate model.posx model.posy model.velx model.vely 0 isAlive4 4
-                posy4new = posyUpdate model.posx model.posy model.velx model.vely isAlive4 4
+                velx1new = velxUpdate model.posx model.posy model.velx model.vely wasdX isAlive1 1 model.platform
+                posx1new = posxUpdate model.posx model.posy model.velx model.vely isAlive1 1 model.platform
+                vely1new = velyUpdate model.posx model.posy model.velx model.vely wasdY isAlive1 1 model.platform
+                posy1new = posyUpdate model.posx model.posy model.velx model.vely isAlive1 1 model.platform
+                velx2new = velxUpdate model.posx model.posy model.velx model.vely arrowX isAlive2 2 model.platform
+                posx2new = posxUpdate model.posx model.posy model.velx model.vely isAlive2 2 model.platform
+                vely2new = velyUpdate model.posx model.posy model.velx model.vely arrowY isAlive2 2 model.platform
+                posy2new = posyUpdate model.posx model.posy model.velx model.vely isAlive2 2 model.platform
+                velx3new = velxUpdate model.posx model.posy model.velx model.vely 0 isAlive3 3 model.platform -- Will implement multiplayer
+                posx3new = posxUpdate model.posx model.posy model.velx model.vely isAlive3 3 model.platform
+                vely3new = velyUpdate model.posx model.posy model.velx model.vely 0 isAlive3 3 model.platform
+                posy3new = posyUpdate model.posx model.posy model.velx model.vely isAlive3 3 model.platform
+                velx4new = velxUpdate model.posx model.posy model.velx model.vely 0 isAlive4 4 model.platform
+                posx4new = posxUpdate model.posx model.posy model.velx model.vely isAlive4 4 model.platform
+                vely4new = velyUpdate model.posx model.posy model.velx model.vely 0 isAlive4 4 model.platform
+                posy4new = posyUpdate model.posx model.posy model.velx model.vely isAlive4 4 model.platform
             in ({model | posx = ((posx1new,posx2new),(posx3new,posx4new)), velx = ((velx1new,velx2new),(velx3new,velx4new)), posy = ((posy1new,posy2new),(posy3new,posy4new)), vely = ((vely1new,vely2new),(vely3new,vely4new))}, Cmd.none)
 
-        SaveTap -> ({model | posy = ((0,0),(0,0)), posx = ((0,0),(0,0))}, Cmd.none)
+        SaveTap -> ({ model | posy = ((0,0),(0,0)), posx = ((0,0),(0,0)) }, Cmd.none)
         LoadTap -> (model, Cmd.none)
-        NewTap -> (model, Cmd.none)
+        NewTap -> ({ model | platform = [] }, Cmd.none)
 
         MakeRequest req -> (model, Cmd.none)
         UrlChange url -> (model, Cmd.none)
@@ -124,6 +128,14 @@ serverSave myUser myPass myAgain =
         , expect = Http.expectString GotText
         }
 
+serverLoad : String -> String -> String -> Cmd Msg
+serverLoad myUser myPass myAgain =
+    Http.post
+        { url = "https://mac1xa3.ca/e/zhua15/lab7/"
+        , body = Http.stringBody "application/x-www-form-urlencoded" ("user="++myUser++"&password="++myPass++"&passwordagain="++myAgain)
+        , expect = Http.expectString GotText
+        }
+
 view : Model -> { title : String, body : Collage Msg }
 view model = 
     let
@@ -145,7 +157,7 @@ view model =
         playerModel3 = playerPiece model 3
         playerModel4 = playerPiece model 4
 
-        platforms = (platformGenerate model platform)
+        platforms = (platformGenerate model.platform)
 
         buttons = [group[save,load,new,saveText,loadText,newText]]   
         saveText = text "Save" |> size 3 |> filled black |> move (-48, 7) |> notifyTap SaveTap
@@ -237,8 +249,8 @@ playerPiece model playernum =
 --                else
 --                   [(posx1-(cubesize/2),posx1+(cubesize/2),posy1),(posx2-(cubesize/2),posx2+(cubesize/2),posy2),(posx3-(cubesize/2),posx3+(cubesize/2),posy3)]
 
-velxUpdate : ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> Float -> Bool -> Int -> Float
-velxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) key isAlive num =    
+velxUpdate : FourTuples -> FourTuples -> FourTuples -> FourTuples -> Float -> Bool -> Int -> PlatformTuples -> Float
+velxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) key isAlive num myPlatforms =    
     let
         posx =  if num == 1 then posx1 else if num == 2 then posx2 else if num == 3 then posx3 else posx4
         posy =  if num == 1 then posy1 else if num == 2 then posy2 else if num == 3 then posy3 else posy4
@@ -253,7 +265,7 @@ velxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         0
     else if abs(velx) < 0.03 then
         0 + a*key
-    else if helperVelx posx posy velx platform then
+    else if helperVelx posx posy velx myPlatforms then
         0
     else if velx > 0 then
         velx + a*key - friction
@@ -262,8 +274,8 @@ velxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
     else
         velx + a*key
 
-velyUpdate : ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> Float -> Bool -> Int -> Float
-velyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) key isAlive num =    
+velyUpdate : FourTuples -> FourTuples -> FourTuples -> FourTuples -> Float -> Bool -> Int -> PlatformTuples -> Float
+velyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) key isAlive num myPlatforms =    
     let
         posx =  if num == 1 then posx1 else if num == 2 then posx2 else if num == 3 then posx3 else posx4
         posy =  if num == 1 then posy1 else if num == 2 then posy2 else if num == 3 then posy3 else posy4
@@ -272,7 +284,7 @@ velyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
     in 
     if isAlive == False then
         0
-    else if key == 1 && (posy == -(39.5 - (cubesize/2)) || isPlatform posx posy platform) then
+    else if key == 1 && (posy == -(39.5 - (cubesize/2)) || isPlatform posx posy myPlatforms) then
         jumpStrength
     else
         if posy +vely >= (39.5 - (cubesize/2)) then
@@ -280,15 +292,15 @@ velyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         else if posy +vely < -(39.5 - (cubesize/2)) then
             0
         else
-            if helperVelyT posx posy vely platform then
+            if helperVelyT posx posy vely myPlatforms then
                 0-g
-            else if helperVelyB posx posy vely platform then
+            else if helperVelyB posx posy vely myPlatforms then
                 0-g
             else
                 vely-g
 
-posxUpdate : ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> Bool -> Int -> Float
-posxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) isAlive num =    
+posxUpdate : FourTuples -> FourTuples -> FourTuples -> FourTuples -> Bool -> Int -> PlatformTuples -> Float
+posxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) isAlive num myPlatforms =    
     let
         posx =  if num == 1 then posx1 else if num == 2 then posx2 else if num == 3 then posx3 else posx4
         posy =  if num == 1 then posy1 else if num == 2 then posy2 else if num == 3 then posy3 else posy4
@@ -303,8 +315,8 @@ posxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         (39.5 - (cubesize/2))
     else
         let
-            (isTrueL,numL) = helperPosxL posx posy velx platform
-            (isTrueR,numR) = helperPosxR posx posy velx platform
+            (isTrueL,numL) = helperPosxL posx posy velx myPlatforms
+            (isTrueR,numR) = helperPosxR posx posy velx myPlatforms
         in
         if isTrueL then
             numL
@@ -313,8 +325,8 @@ posxUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         else
             posx + velx
 
-posyUpdate : ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) -> ((Float,Float),(Float,Float)) ->Bool -> Int -> Float
-posyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) isAlive num =    
+posyUpdate : FourTuples -> FourTuples -> FourTuples -> FourTuples ->Bool -> Int -> PlatformTuples -> Float
+posyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,velx2),(velx3,velx4)) ((vely1,vely2),(vely3,vely4)) isAlive num myPlatforms =    
     let
         posx =  if num == 1 then posx1 else if num == 2 then posx2 else if num == 3 then posx3 else posx4
         posy =  if num == 1 then posy1 else if num == 2 then posy2 else if num == 3 then posy3 else posy4
@@ -329,9 +341,9 @@ posyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         (39.5 - (cubesize/2))
     else
         let
-            (isTrueT,numT) = helperPosyT posx posy vely platform
-            (isTrueB,numB) = helperPosyB posx posy vely platform
-            (isTrueC,numC) = clipperCorner posx posy velx vely platform
+            (isTrueT,numT) = helperPosyT posx posy vely myPlatforms
+            (isTrueB,numB) = helperPosyB posx posy vely myPlatforms
+            (isTrueC,numC) = clipperCorner posx posy velx vely myPlatforms
         in 
         if isTrueT then
             numT
@@ -342,7 +354,7 @@ posyUpdate ((posx1,posx2),(posx3,posx4)) ((posy1,posy2),(posy3,posy4)) ((velx1,v
         else
             posy + vely
 
-helperVelx : Float -> Float -> Float ->List((Float,Float),(Float,Float),Color) -> Bool   
+helperVelx : Float -> Float -> Float -> PlatformTuples -> Bool   
 helperVelx posx posy velx x = 
     let
         header = case head(x) of
@@ -357,7 +369,7 @@ helperVelx posx posy velx x =
         else
             helperVelx posx posy velx x2 || posy > ymin - cubesize/2 && posy < ymax + cubesize/2 && (posx == xmin - (cubesize/2) || posx == xmax + (cubesize/2))
 
-helperVelyT : Float -> Float -> Float ->List((Float,Float),(Float,Float),Color) -> Bool
+helperVelyT : Float -> Float -> Float -> PlatformTuples -> Bool
 helperVelyT posx posy vely x = 
     let
         header = case head(x) of
@@ -372,7 +384,7 @@ helperVelyT posx posy vely x =
         else
             (helperVelyT posx posy vely x2) || (posy == ymax + cubesize/2 && posx > xmin - (cubesize/2) && posx < xmax + (cubesize/2) && vely < 0)
 
-helperVelyB : Float -> Float -> Float ->List((Float,Float),(Float,Float),Color) -> Bool
+helperVelyB : Float -> Float -> Float ->PlatformTuples -> Bool
 helperVelyB posx posy vely x = 
     let
         header = case head(x) of
@@ -387,7 +399,7 @@ helperVelyB posx posy vely x =
         else
             (helperVelyB posx posy vely x2) || (posy == ymin - cubesize/2 && posx > xmin - (cubesize/2) && posx < xmax + (cubesize/2) && vely > 0)
 
-helperPosxL : Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> (Bool,Float)
+helperPosxL : Float -> Float -> Float -> PlatformTuples -> (Bool,Float)
 helperPosxL posx posy velx x = 
     let
         header = case head(x) of
@@ -404,7 +416,7 @@ helperPosxL posx posy velx x =
         else
             (helperPosxL posx posy velx x2)
 
-helperPosxR : Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> (Bool,Float)
+helperPosxR : Float -> Float -> Float -> PlatformTuples -> (Bool,Float)
 helperPosxR posx posy velx x = 
     let
         header = case head(x) of
@@ -421,7 +433,7 @@ helperPosxR posx posy velx x =
         else
             (helperPosxR posx posy velx x2)
 
-helperPosyT : Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> (Bool,Float)
+helperPosyT : Float -> Float -> Float -> PlatformTuples -> (Bool,Float)
 helperPosyT posx posy vely x = 
     let
         header = case head(x) of
@@ -438,7 +450,7 @@ helperPosyT posx posy vely x =
         else
             (helperPosyT posx posy vely x2)
 
-helperPosyB : Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> (Bool,Float)
+helperPosyB : Float -> Float -> Float -> PlatformTuples -> (Bool,Float)
 helperPosyB posx posy vely x = 
     let
         header = case head(x) of
@@ -455,7 +467,7 @@ helperPosyB posx posy vely x =
         else
             (helperPosyB posx posy vely x2)
 
-clipperCorner : Float -> Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> (Bool,Float)
+clipperCorner : Float -> Float -> Float -> Float -> PlatformTuples -> (Bool,Float)
 clipperCorner posx posy velx vely x = 
     let
         header = case head(x) of
@@ -475,7 +487,7 @@ clipperCorner posx posy velx vely x =
         else
             (clipperCorner posx posy velx vely x2)
 
-helperCornerV : Float -> Float -> Float -> Float -> List((Float,Float),(Float,Float),Color) -> Bool
+helperCornerV : Float -> Float -> Float -> Float -> PlatformTuples -> Bool
 helperCornerV posx posy velx vely x = 
     let
         header = case head(x) of
@@ -492,7 +504,7 @@ helperCornerV posx posy velx vely x =
         else
             (helperCornerV posx posy velx vely x2)
 
-isPlatform : Float -> Float -> List((Float,Float),(Float,Float),Color) -> Bool
+isPlatform : Float -> Float -> PlatformTuples -> Bool
 isPlatform posx posy x =
     let
         header = case head(x) of
@@ -509,20 +521,20 @@ isPlatform posx posy x =
         else
             isPlatform posx posy x2
 
-platformGenerate : Model -> List((Float,Float),(Float,Float),Color) -> List (Shape userMsg)
-platformGenerate model x = 
+platformGenerate : PlatformTuples -> List (Shape userMsg)
+platformGenerate platformList = 
     let
-        header =  case head(x) of
+        header =  case head(platformList) of
                     Just v -> v
                     Nothing -> ((0,0),(0,0),blank)
 
         ((xmin,xmax),(ymin,ymax),colour) = header
         
-        x2 = case tail(x) of
+        x2 = case tail(platformList) of
                 Just v -> v
                 Nothing -> []
     in 
     if x2 /= [] then
-        [rectangle (abs(xmax - xmin)) (abs(ymax - ymin)) |> filled colour |> move (((xmin + xmax)/2),((ymax + ymin)/2))] ++ platformGenerate model x2
+        [rectangle (abs(xmax - xmin)) (abs(ymax - ymin)) |> filled colour |> move (((xmin + xmax)/2),((ymax + ymin)/2))] ++ platformGenerate x2
     else
         [rectangle (abs(xmax - xmin)) (abs(ymax - ymin)) |> filled colour |> move (((xmin + xmax)/2),((ymax + ymin)/2))]
